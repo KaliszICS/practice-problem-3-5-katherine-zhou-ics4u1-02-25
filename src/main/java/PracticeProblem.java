123781a17a97c17b79eb7112b6f7a26e8bd58c4f
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-06-11 05:27:11 +0000

    Update Praactice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,79 @@
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		String[][] maze = {
+			{"", "", "", "", ""},
+			{"", "", "", "", ""},
+			{"", "", "", "F", ""},
+			{"S", "", "", "", ""},
+		  };
+		  System.out.println(noOfPaths(maze));
 
+	}
+	public static int searchMazeMoves(String[][] arr) {
+
+		int row = arr.length - 1;
+		int col = 0;
+
+		int noOfMoves = 0;
+
+		return dfsHelper(arr, row, col, noOfMoves);
+		
+	}
+
+	public static int dfsHelper(String[][] arr, int row, int col, int moves) {
+
+		if(row<0 || col>arr[row].length-1){
+			return -1;
+		}
+		if (arr[row][col].equals("F")){
+			return moves;
+		}
+
+		int right = dfsHelper(arr, row, col+1, moves+1);
+		int up = dfsHelper(arr, row-1,col, moves+1);
+		if (right < 0) {
+			return up;
+		}
+		else if (up < 0) {
+			return right;
+		}
+		return Math.min(up, right);
+	}
+
+
+
+	public static int noOfPaths(String [][] arr){
+		int row =arr.length-1;
+		int col=0;
+		int noOfPaths =0;
+		return noOfPathsHelper(row, col, arr, noOfPaths);
+		
+
+	}
+	public static int noOfPathsHelper(int row, int col,String[][] arr, int paths){
+		if (row<0 || col>arr[row].length-1){
+			return -1;
+		}
+		if (arr[row][col].equals("F")){
+			System.out.println(paths);
+			paths++;
+			return paths;
+		}
+		int up= noOfPathsHelper(row-1, col, arr, paths);
+		int right= noOfPathsHelper(row, col+1, arr, paths);
+		if(up<0){
+			paths--;
+			return right;
+		}
+		if (right<0){
+			paths--;
+			return up;
+		}
+		else{
+			return up+right;
+		}
 	}
 
 	
